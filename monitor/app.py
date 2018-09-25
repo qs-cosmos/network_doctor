3df67bfa492f9a149bf5bd54492685a50bca3c9f
qs_cosmos <qs.cosmos.pine@gmail.com>
2018-09-25 18:37:41 +0800

    Modify the way to read configure files.

--- monitor/app.py
+++ monitor/app.py
@@ -3,6 +3,8 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from client.interface import MainWindow
 from client.spider import Spider
+from client.analyzer import Analyzer
+from client.network import Reporter
 from config.runtime import RUNTIME
 
 if __name__ == '__main__':
@@ -10,7 +12,12 @@ if __name__ == '__main__':
     # 开启数据监测线程
     spider = Spider()
     spider.run()
-
+    # 数据分析
+    analyser = Analyzer()
+    analyser.run()
+    # 网络通信
+    reporter = Reporter()
+    reporter.run()
     # 运行 客户端 界面
     app = QApplication(sys.argv)
     QApplication.setQuitOnLastWindowClosed(False)
