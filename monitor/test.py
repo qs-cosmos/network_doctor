426aebd27304a2c6c719d694c5a43520b8a5c990
yuqingsong <yuqingsong@bytedance.com>
2018-09-25 10:00:47 +0000

    Untracked the config/runtime.json
    Change-Id: I3d71f6043613bc9d9cf434e601dedabf0674bf3b

--- monitor/test.py
+++ monitor/test.py
@@ -1,14 +1,3 @@
 # coding: utf-8
 
-from config.constant import FILE
-from config.runtime import RUNTIME
-from core.packet.ip import REGEX
-
-print(FILE.module(__file__))
-
-RUNTIME.load()
-print RUNTIME.CHECK_LIST
-
-RUNTIME.add('www.baidu.com')
-RUNTIME.delete('www.baidu.com')
-RUNTIME.update()
+from client.analyzer
